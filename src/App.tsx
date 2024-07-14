e79cb9e8f448bee3d5a6526b7160aa59e6f8c3fc
Florent Klein <florent.klein@dametis.com>
2024-07-14 02:15:56 +0200

    before compiler

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function Header() {
+  console.log("Header", Math.random());
+  return (
+    <header>
+      <h1>React Counter</h1>
+    </header>
+  );
+}
+
+export function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+      <div>
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+    </>
+  );
+}
